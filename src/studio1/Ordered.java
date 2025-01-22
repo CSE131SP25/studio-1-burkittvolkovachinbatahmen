1f01f9d76b464e432859359e64876b3927390da5
oyuchinbat <oyuchinbat@localhost>
2025-01-22 14:18:21 -0600

    Studio 0 Completion

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,8 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean isOrdered= x>y && y>z || y<z && x<y;
+		System.out.println(isOrdered);
 	}
 
 }
